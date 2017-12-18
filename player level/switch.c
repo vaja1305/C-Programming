88c3179916495cf87b0ef46723ff9f8f61f824dd
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2017-12-18 23:04:59 +0530

    Create switch.c

---
+++ player level/switch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int i = 1;
+switch(i)
+{
+ printf("This is c program.");
+ case 1:
+ printf("Case1");
+ break;
+ case 2:
+ printf("Case2");
+ break;
+}
+return 0;
+}
