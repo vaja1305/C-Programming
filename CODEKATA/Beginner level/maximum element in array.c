9245ccd958b265319ce3918b5e55e2cd43627111
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-02 23:06:51 +0530

    Create maximum element in array.c

---
+++ CODEKATA/Beginner level/maximum element in array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+    int n,a[10],max=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+	return 0;
+}
