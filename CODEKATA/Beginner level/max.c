aba8b990ad5db04dbdbdd393efe763be6db33219
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-06 22:27:21 +0530

    Create max.c

---
+++ CODEKATA/Beginner level/max.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int a[10],max=0,n,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]>max)
+	    {
+	        max=a[i];
+	    }
+	}
+	printf("%d",max);
+	return 0;
+}
