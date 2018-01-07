84f1a914ea7c30d0308802f496d14a4a3832dc1c
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-07 13:53:54 +0530

    Create str  in binary.c

---
+++ CODEKATA/Beginner level/str  in binary.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+	char a[100];
+	int n,i,num,count=0;
+	scanf("%s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]=='0'||a[i]=='1')
+	    {   
+	        count++;
+	    }
+	    else
+	    {
+	       printf("no..");
+	       break;
+	    }
+	}
+	if(n==count)
+	{
+	     printf("yes...it is in binary representation");
+	}
+	return 0;
+}
