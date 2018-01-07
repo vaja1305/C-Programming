ea0e22aa48fd433d86948096f1fad361e3b4fb1f
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-07 15:59:51 +0530

    Create given no is multiple of 7.c

---
+++ CODEKATA/Beginner level/given no is multiple of 7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("yes..multiple of 7");
+    }
+    else
+    {
+        printf("no");   
+    }
+    return 0;
+}
