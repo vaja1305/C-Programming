a338aa78fe2edf43d1a0b7d14a4d1e95a9e0eb5f
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-07 20:27:11 +0530

    Update n's nearest power of 2.c

--- CODEKATA/Beginner level/n's nearest power of 2.c
+++ CODEKATA/Beginner level/n's nearest power of 2.c
@@ -2,18 +2,16 @@
 #include<math.h>
 int main() 
 {
-    int n,a,b,c,i;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        a=pow(2,i);
-        if(a>n)
-        {
-             b=i;
-             break;
-        }
-    }
-    c=pow(2,b);
-    printf("%d",c);
-    return 0;
+    int n,i,a;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	    a=pow(2,i);
+	    if(a>n)
+	    {
+	        printf("%d",a);
+	        break;
+	    }
+	}
+	return 0;
 }
