e4365acbcb65278f15f96c014ba796553085f62e
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-08 21:58:33 +0530

    Create a)given string print odd and even positions separately.c

---
+++ CODEKATA/Beginner level/a)given string print odd and even positions separately.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{	
+   char a[10];
+   int i,n;
+   scanf("%s",a);
+   n=strlen(a);
+   for(i=0;i<n;i++)
+   {                                    
+      if(i%2==0)
+      {
+          printf("%c",a[i]);
+      }
+   }
+   printf(" ");
+   for(i=0;i<n;i++)
+   {
+       if(i%2!=0)
+       {
+           printf("%c",a[i]);
+       }
+   }
+	return 0;
+}
