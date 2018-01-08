ba02b621608593910cdf0ec8d7ab7407524e3409
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-08 22:33:42 +0530

    Create lcm of 2 nos.c

---
+++ CODEKATA/Beginner level/lcm of 2 nos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int n1,n2,gcd,lcm,i;
+	scanf("%d %d",&n1,&n2);
+	for(i=1;i<=n1&&i<=n2;i++)
+	{
+	  if(n1%i==0 && n2%i==0)        //checks if i is factor of n1 and n2
+	  {
+	      gcd=i;
+	  }
+	}
+	lcm=(n1*n2)/gcd;
+	printf("lcm of %d and %d is %d",n1,n2,lcm);
+	return 0;
+}
