a473dc31b663dffc23310ecb232f9b0b6aacd88c
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-08 23:48:17 +0530

    Update simple interest.c

--- CODEKATA/Beginner level/simple interest.c
+++ CODEKATA/Beginner level/simple interest.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include<math.h>
 int main()
 {
-	int p,n,r,si;
-	scanf("%d %d %d",&p,&n,&r);
-	si=(p*n*r)/100;
-	printf("%d",si);
+	float p,r,si;
+	int n,result;
+	scanf("%f %d %f",&p,&n,&r);
+	si=((p*n*r)/100);
+	result=floor(si);
+	printf("%d",result);
 	return 0;
 }
