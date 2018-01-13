db5279c9c7e4eb393b88a811e44ea4545e9d5e58
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-13 23:42:51 +0530

    Create holiday or not.c

---
+++ CODEKATA/Player level/holiday or not.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	char a[10];
+	scanf("%s",a);
+	if(a[0]=='S'||a[0]=='s')
+	{
+	    printf("yes...holiday");
+	}
+	else
+	{
+	 printf("no..not a holiday");   
+	}
+	return 0;
+}
