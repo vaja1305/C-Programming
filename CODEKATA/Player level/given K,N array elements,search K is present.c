55370810f79d7c8e4ee28a81ffb7c2859061fdaa
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-18 21:45:57 +0530

    Create given K,N array elements,search K is present.c

---
+++ CODEKATA/Player level/given K,N array elements,search K is present.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+	int n,k,i,a[10];
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]==k)
+	    {
+	        printf("yes");
+	    }
+	}
+	return 0;
+}
