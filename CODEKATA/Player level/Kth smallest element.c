6ebcbd8c7b64cd83af9bac06187461ad954d2977
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-20 23:22:32 +0530

    Create Kth smallest element.c

---
+++ CODEKATA/Player level/Kth smallest element.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k,a[10],temp=0;
+    scanf("%d",&n);
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("%d",a[k-1]);
+   return 0;
+}
