35ea48aad70c7a2a8aff6e9866c342b1793de701
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-20 22:19:05 +0530

    Create sum of interior angles of the triangle.c

---
+++ CODEKATA/Player level/sum of interior angles of the triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,sum=0;
+	  scanf("%d %d %d",&a,&b,&c);
+    sum=a+b+c;
+    if(sum==180)
+    {
+        printf("yes..it forms traingle");
+    }
+    else
+    {
+        printf("they cant be the interior angles of the triangle");
+    }
+	return 0;
+}
