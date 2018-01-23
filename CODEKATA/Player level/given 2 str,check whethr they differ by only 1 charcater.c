cf6aae8a281c532dc420433c7b8b7d5f41a1e958
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-23 11:49:09 +0530

    Create given 2 str,check whethr they differ by only 1 charcater.c

---
+++ CODEKATA/Player level/given 2 str,check whethr they differ by only 1 charcater.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{	
+char a[10],b[10];	
+int i,j,n,n1,count=0;	
+scanf("%s %s",a,b);	
+n=strlen(a);	
+n1=strlen(b);	
+if(n==n1)	
+{   
+for(i=0,j=0;i<n,j<n1;i++,j++)   
+{    	    
+if(a[i]==b[j])    	   
+{    	       
+continue;    	  
+}    	   
+else    	  
+{
+count++;    	  
+}   
+}    	
+if(count==1)  
+{    	    p
+printf("yes..differ by 1 character");    
+}	
+}
+else	
+{
+printf("false");
+}	
+return 0;
+}
