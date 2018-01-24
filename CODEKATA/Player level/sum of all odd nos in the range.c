31d78b593df4960623ea48438f32769d82e1fd4f
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-01-24 20:42:37 +0530

    Create sum of all odd nos in the range.c

---
+++ CODEKATA/Player level/sum of all odd nos in the range.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n,i,k,sum=0;
+	scanf("%d %d",&n,&k);
+	for(i=n;i<=k;i++)
+	{
+	    if(i%2==1)
+	    {
+	       sum=sum+i;
+	    }
+	}
+	 printf("%d",sum);
+	return 0;
+}
